184749d61d618753ffff55424d8ebefb8566f517
Armando <jarmando.chacon36@gmail.com>
2020-02-15 00:28:42 -0500

    Created class to make calls to the api using Retrofit

---
+++ app/src/main/java/com/example/lastfmtest/injection/NetworkModule.kt
@@ -0,0 +1,43 @@
+package com.example.lastfmtest.injection
+
+import com.example.lastfmtest.network.Api
+import com.example.lastfmtest.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+// Safe here as we are dealing with a Dagger 2 module
+@Suppress("unused")
+object NetworkModule {
+    /**
+     * Provides the Post service implementation.
+     * @param retrofit the Retrofit object used to instantiate the service
+     * @return the Post service implementation.
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun providePostApi(retrofit: Retrofit): Api {
+        return retrofit.create(Api::class.java)
+    }
+
+    /**
+     * Provides the Retrofit object.
+     * @return the Retrofit object
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+    }
+}
