4b6af35a1c02907f649416f464dbb6aab8fbe7e2
Armando <jarmando.chacon36@gmail.com>
2020-02-15 01:04:29 -0500

    Added RxJava library to make api calls asynchronously and handle the connection and response states

--- app/src/main/java/com/example/lastfmtest/ui/ApiViewModel.kt
+++ app/src/main/java/com/example/lastfmtest/ui/ApiViewModel.kt
@@ -2,10 +2,51 @@ package com.example.lastfmtest.ui
 
 import com.example.lastfmtest.base.BaseViewModel
 import com.example.lastfmtest.network.Api
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ApiViewModel():BaseViewModel() {
     @Inject
     lateinit var api: Api
+    private lateinit var subscription: Disposable
+
+    init{
+        loadArtists()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
+
+    private fun loadArtists() {
+        subscription = api.getArtists()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onStart() }
+            .doOnTerminate { onFinish() }
+            .subscribe(
+                { onSuccess() },
+                { onError() }
+            )
+    }
+
+    private fun onStart(){
+
+    }
+
+    private fun onFinish(){
+
+    }
+
+    private fun onSuccess(){
+
+    }
+
+    private fun onError(){
+
+    }
 
 }
